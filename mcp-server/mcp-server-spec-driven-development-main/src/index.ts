afb55e2719d51f21b9b018fe019a80a3352f7eeb
Bryan Leow K.H <bryankhleow@gmail.com>
2026-01-04 11:55:38 +0800

    docs: go through https://modelcontextprotocol.io/docs/develop/build-server

--- mcp-server/mcp-server-spec-driven-development-main/src/index.ts
+++ mcp-server/mcp-server-spec-driven-development-main/src/index.ts
@@ -3,10 +3,13 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { createServer } from "./server.js";
 
+// standard file from tutorial https://modelcontextprotocol.io/docs/develop/build-server#typescript
 async function main() {
   const server: McpServer = createServer();
   const transport = new StdioServerTransport();
   await server.connect(transport);
+  // use .error not .log as that does into stdio
+  console.error("spec-driven MCP Server running on stdio");
 }
 
 main().catch((error) => {
